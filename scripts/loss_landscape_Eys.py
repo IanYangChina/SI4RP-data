d06cc7dd79f8ec6eb192aff972370043184bf8c0
xintong <xintongyang1994@foxmail.com>
2023-12-11 08:14:32 +0000

    grads

--- scripts/loss_landscape_Eys.py
+++ scripts/loss_landscape_Eys.py
@@ -129,6 +129,7 @@ p_density_str = '6e7pd'
 
 loss_cfg = {
     'exponential_distance': False,
+    'averaging_loss': True,
     'point_distance_rs_loss': True,
     'point_distance_sr_loss': False,
     'down_sample_voxel_size': 0.005,

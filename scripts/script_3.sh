08e0dc7c0581df32ebfcab4cb72d82460705c1e5
xintong <xintongyang1994@foxmail.com>
2024-01-26 15:25:34 +0000

    results

--- scripts/script_3.sh
+++ scripts/script_3.sh
@@ -1,18 +1,56 @@
-python gradients_compute.py --pd_rs_loss --pd_sr_loss --fewshot
-python gradients_compute.py --pd_rs_loss --fewshot
-python gradients_compute.py --pd_sr_loss --fewshot
-python gradients_compute.py --prd_rs_loss --prd_sr_loss --fewshot
-python gradients_compute.py --prd_rs_loss --fewshot
-python gradients_compute.py --prd_sr_loss --fewshot
-python gradients_compute.py --hm_loss --fewshot
-python gradients_compute.py --emd_p_loss --fewshot
-python gradients_compute.py --emd_pr_loss --fewshot
-python gradients_compute.py --pd_rs_loss --pd_sr_loss --exp_dist --fewshot
-python gradients_compute.py --pd_rs_loss --exp_dist --fewshot
-python gradients_compute.py --pd_sr_loss --exp_dist --fewshot
-python gradients_compute.py --prd_rs_loss --prd_sr_loss --exp_dist --fewshot
-python gradients_compute.py --prd_rs_loss --exp_dist --fewshot
-python gradients_compute.py --prd_sr_loss --exp_dist --fewshot
-python gradients_compute.py --hm_loss --exp_dist --fewshot
-python gradients_compute.py --emd_p_loss --exp_dist --fewshot
-python gradients_compute.py --emd_pr_loss --exp_dist --fewshot
+python gradients_compute.py --pd_rs_loss --pd_sr_loss --fewshot --param_set 1
+python gradients_compute.py --pd_rs_loss --fewshot --param_set 1
+python gradients_compute.py --pd_sr_loss --fewshot --param_set 1
+python gradients_compute.py --prd_rs_loss --prd_sr_loss --fewshot --param_set 1
+python gradients_compute.py --prd_rs_loss --fewshot --param_set 1
+python gradients_compute.py --prd_sr_loss --fewshot --param_set 1
+python gradients_compute.py --hm_loss --fewshot --param_set 1
+python gradients_compute.py --emd_p_loss --fewshot --param_set 1
+python gradients_compute.py --emd_pr_loss --fewshot --param_set 1
+python gradients_compute.py --pd_rs_loss --pd_sr_loss --exp_dist --fewshot --param_set 1
+python gradients_compute.py --pd_rs_loss --exp_dist --fewshot --param_set 1
+python gradients_compute.py --pd_sr_loss --exp_dist --fewshot --param_set 1
+python gradients_compute.py --prd_rs_loss --prd_sr_loss --exp_dist --fewshot --param_set 1
+python gradients_compute.py --prd_rs_loss --exp_dist --fewshot --param_set 1
+python gradients_compute.py --prd_sr_loss --exp_dist --fewshot --param_set 1
+python gradients_compute.py --hm_loss --exp_dist --fewshot --param_set 1
+python gradients_compute.py --emd_p_loss --exp_dist --fewshot --param_set 1
+python gradients_compute.py --emd_pr_loss --exp_dist --fewshot --param_set 1
+# non-fewshot param_set 0
+python gradients_compute.py --pd_rs_loss --pd_sr_loss
+python gradients_compute.py --pd_rs_loss
+python gradients_compute.py --pd_sr_loss
+python gradients_compute.py --prd_rs_loss --prd_sr_loss
+python gradients_compute.py --prd_rs_loss
+python gradients_compute.py --prd_sr_loss
+python gradients_compute.py --hm_loss
+python gradients_compute.py --emd_p_loss
+python gradients_compute.py --emd_pr_loss
+python gradients_compute.py --pd_rs_loss --pd_sr_loss --exp_dist
+python gradients_compute.py --pd_rs_loss --exp_dist
+python gradients_compute.py --pd_sr_loss --exp_dist
+python gradients_compute.py --prd_rs_loss --prd_sr_loss --exp_dist
+python gradients_compute.py --prd_rs_loss --exp_dist
+python gradients_compute.py --prd_sr_loss --exp_dist
+python gradients_compute.py --hm_loss --exp_dist
+python gradients_compute.py --emd_p_loss --exp_dist
+python gradients_compute.py --emd_pr_loss --exp_dist
+# non-fewshot, param_set 1
+python gradients_compute.py --pd_rs_loss --pd_sr_loss --param_set 1
+python gradients_compute.py --pd_rs_loss --param_set 1
+python gradients_compute.py --pd_sr_loss --param_set 1
+python gradients_compute.py --prd_rs_loss --prd_sr_loss --param_set 1
+python gradients_compute.py --prd_rs_loss --param_set 1
+python gradients_compute.py --prd_sr_loss --param_set 1
+python gradients_compute.py --hm_loss --param_set 1
+python gradients_compute.py --emd_p_loss --param_set 1
+python gradients_compute.py --emd_pr_loss --param_set 1
+python gradients_compute.py --pd_rs_loss --pd_sr_loss --exp_dist --param_set 1
+python gradients_compute.py --pd_rs_loss --exp_dist --param_set 1
+python gradients_compute.py --pd_sr_loss --exp_dist --param_set 1
+python gradients_compute.py --prd_rs_loss --prd_sr_loss --exp_dist --param_set 1
+python gradients_compute.py --prd_rs_loss --exp_dist --param_set 1
+python gradients_compute.py --prd_sr_loss --exp_dist --param_set 1
+python gradients_compute.py --hm_loss --exp_dist --param_set 1
+python gradients_compute.py --emd_p_loss --exp_dist --param_set 1
+python gradients_compute.py --emd_pr_loss --exp_dist --param_set 1

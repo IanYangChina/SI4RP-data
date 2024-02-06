81ecae0e81f14a6020bfe0ce241f04e360b2c14b
xintong <xintongyang1994@foxmail.com>
2024-02-06 08:57:44 +0000

    new range for nu

--- scripts/optimise.py
+++ scripts/optimise.py
@@ -78,7 +78,7 @@ def main(arguments):
 
     # Parameter ranges
     E_range = (1e4, 3e5)
-    nu_range = (0.01, 0.49)
+    nu_range = (0.01, 0.48)
     yield_stress_range = (1e3, 2e4)
     rho_range = (1000, 2000)
     mf_range = (0.01, 2.0)

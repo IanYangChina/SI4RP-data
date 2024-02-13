c0c63e5b7b9750cdde953a729d830145b363eb12
xintong <xintongyang1994@foxmail.com>
2024-02-13 09:08:41 +0000

    results

--- scripts/script_loss_3.sh
+++ scripts/script_loss_3.sh
@@ -1,4 +1,4 @@
-python loss_landscape_mfgf.py --backend cuda
-python loss_landscape_mfgf.py --exp_dist --backend cuda
-python loss_landscape_mfgf.py --backend cuda --fewshot
-python loss_landscape_mfgf.py --exp_dist --backend cuda --fewshot
+python loss_landscape_mfgf.py --backend opengl
+python loss_landscape_mfgf.py --exp_dist --backend opengl
+python loss_landscape_mfgf.py --backend opengl --fewshot
+python loss_landscape_mfgf.py --exp_dist --backend opengl --fewshot

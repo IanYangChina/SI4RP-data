b1c6ed6e6e1c6e3525ec2e0640eb1f3f8f911214
Xintong <xintongyang1994@foxmail.com>
2024-02-16 16:37:24 +0000

    bash scripts

--- scripts/script_optim_1.sh
+++ scripts/script_optim_1.sh
@@ -1,4 +1,6 @@
+python optimise.py --fewshot --param_set 0 --backend cuda --prd_sr_loss --prd_rs_loss --emd_pr_loss
 python optimise.py --fewshot --param_set 1 --backend cuda --n_run 0 --seed 2 --emd_pr_loss
+python optimise.py --fewshot --param_set 1 --backend cuda --n_run 1 --seed 1 --emd_p_loss
 python optimise.py --fewshot --param_set 1 --backend cuda --n_run 1 --seed 2 --emd_p_loss
 python optimise.py --fewshot --param_set 1 --backend cuda --n_run 2 --seed 2 --pd_rs_loss --pd_sr_loss
 python optimise.py --fewshot --param_set 1 --backend cuda --n_run 3 --seed 0 --prd_rs_loss --prd_sr_loss

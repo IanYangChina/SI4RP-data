adfca2e1378ec1454105175edabe58309c414c81
xintong <xintongyang1994@foxmail.com>
2024-03-05 09:21:02 +0000

    new scripts

--- scripts/script_optim_2090-0.sh
+++ scripts/script_optim_2090-0.sh
@@ -1,7 +1,3 @@
-python optimise.py --realoneshot --backend cuda --param_set 1 --realoneshot_agent_id 1 --n_run 1 --seed 0 --prd_rs_loss --prd_sr_loss
-python optimise.py --realoneshot --backend cuda --param_set 1 --realoneshot_agent_id 1 --n_run 1 --seed 1 --prd_rs_loss --prd_sr_loss
-python optimise.py --realoneshot --backend cuda --param_set 1 --realoneshot_agent_id 1 --n_run 1 --seed 2 --prd_rs_loss --prd_sr_loss
-python optimise.py --realoneshot --backend cuda --param_set 0 --realoneshot_agent_id 0 --n_run 11 --seed 0 --pd_rs_loss --pd_sr_loss --emd_pr_loss
 python optimise.py --realoneshot --backend cuda --param_set 0 --realoneshot_agent_id 0 --n_run 11 --seed 1 --pd_rs_loss --pd_sr_loss --emd_pr_loss
 python optimise.py --realoneshot --backend cuda --param_set 0 --realoneshot_agent_id 0 --n_run 11 --seed 2 --pd_rs_loss --pd_sr_loss --emd_pr_loss
 python optimise.py --realoneshot --backend cuda --param_set 0 --realoneshot_agent_id 2 --n_run 11 --seed 1 --pd_rs_loss --pd_sr_loss --emd_pr_loss
--- scripts/script_optim_2090.sh
+++ scripts/script_optim_2090.sh
@@ -1,5 +1,4 @@
-python optimise.py --realoneshot --backend cuda --param_set 0 --realoneshot_agent_id 1 --n_run 11 --seed 0 --pd_rs_loss --pd_sr_loss --emd_pr_loss
-python optimise.py --realoneshot --backend cuda --param_set 0 --realoneshot_agent_id 1 --n_run 11 --seed 1 --pd_rs_loss --pd_sr_loss --emd_pr_loss
-python optimise.py --realoneshot --backend cuda --param_set 0 --realoneshot_agent_id 1 --n_run 11 --seed 2 --pd_rs_loss --pd_sr_loss --emd_pr_loss
-python optimise.py --realoneshot --backend cuda --param_set 0 --realoneshot_agent_id 2 --n_run 11 --seed 0 --pd_rs_loss --pd_sr_loss --emd_pr_loss
-python optimise.py --oneshot --param_set 0 --backend cuda --n_run 11 --seed 1 --emd_pr_loss --pd_sr_loss --pd_rs_loss
+python optimise.py --realoneshot --backend cuda --param_set 1 --realoneshot_agent_id 1 --n_run 1 --seed 0 --prd_rs_loss --prd_sr_loss
+python optimise.py --realoneshot --backend cuda --param_set 1 --realoneshot_agent_id 1 --n_run 1 --seed 1 --prd_rs_loss --prd_sr_loss
+python optimise.py --realoneshot --backend cuda --param_set 1 --realoneshot_agent_id 1 --n_run 1 --seed 2 --prd_rs_loss --prd_sr_loss
+python optimise.py --realoneshot --backend cuda --param_set 0 --realoneshot_agent_id 0 --n_run 11 --seed 0 --pd_rs_loss --pd_sr_loss --emd_pr_loss

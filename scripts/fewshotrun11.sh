2d38b1269f8923a538c169f90ae1280c831d08e8
xintong <xintongyang1994@foxmail.com>
2024-03-06 09:15:14 +0000

    scirpt

---
+++ scripts/fewshotrun11.sh
@@ -0,0 +1,3 @@
+python optimise.py --fewshot --run 11 --seed 0 --backend cuda --param_set 0 --pd_rs_loss --pd_sr_loss --emd_pr_loss
+python optimise.py --fewshot --run 11 --seed 1 --backend cuda --param_set 0 --pd_rs_loss --pd_sr_loss --emd_pr_loss
+python optimise.py --fewshot --run 11 --seed 2 --backend cuda --param_set 0 --pd_rs_loss --pd_sr_loss --emd_pr_loss

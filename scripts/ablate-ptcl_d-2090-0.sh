d596e3fae4f1ec9ab3a016697ae296f49478a2cd
xintong <xintongyang1994@foxmail.com>
2024-03-07 09:28:10 +0000

    ptcl_d scripts

--- scripts/ablate-ptcl_d-2090-0.sh
+++ scripts/ablate-ptcl_d-2090-0.sh
@@ -1,7 +1,7 @@
-python optimise.py --fewshot --n_run 12 --seed 0 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 6e7 --dsvs 0.0035
-python optimise.py --fewshot --n_run 12 --seed 1 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 6e7 --dsvs 0.0035
-python optimise.py --fewshot --n_run 12 --seed 2 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 6e7 --dsvs 0.0035
-python optimise.py --realoneshot --n_run 12 --seed 0  --backend cuda --param_set 1 --realoneshot_agent_id 0 --pd_rs_loss --pd_sr_loss --ptcl_d 6e7 --dsvs 0.0035
-python optimise.py --realoneshot --n_run 12 --seed 1  --backend cuda --param_set 1 --realoneshot_agent_id 0 --pd_rs_loss --pd_sr_loss --ptcl_d 6e7 --dsvs 0.0035
-python optimise.py --realoneshot --n_run 12 --seed 2  --backend cuda --param_set 1 --realoneshot_agent_id 0 --pd_rs_loss --pd_sr_loss --ptcl_d 6e7 --dsvs 0.0035
-python optimise.py --realoneshot --n_run 12 --seed 0  --backend cuda --param_set 1 --realoneshot_agent_id 1 --pd_rs_loss --pd_sr_loss --ptcl_d 6e7 --dsvs 0.0035
+python optimise.py --fewshot --n_run 13 --seed 0 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 8e7 --dsvs 0.003
+python optimise.py --fewshot --n_run 13 --seed 1 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 8e7 --dsvs 0.003
+python optimise.py --fewshot --n_run 13 --seed 2 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 8e7 --dsvs 0.003
+python optimise.py --realoneshot --n_run 13 --seed 0  --backend cuda --param_set 1 --realoneshot_agent_id 0 --pd_rs_loss --pd_sr_loss --ptcl_d 8e7 --dsvs 0.003
+python optimise.py --realoneshot --n_run 13 --seed 1  --backend cuda --param_set 1 --realoneshot_agent_id 0 --pd_rs_loss --pd_sr_loss --ptcl_d 8e7 --dsvs 0.003
+python optimise.py --realoneshot --n_run 13 --seed 2  --backend cuda --param_set 1 --realoneshot_agent_id 0 --pd_rs_loss --pd_sr_loss --ptcl_d 8e7 --dsvs 0.003
+python optimise.py --realoneshot --n_run 13 --seed 0  --backend cuda --param_set 1 --realoneshot_agent_id 1 --pd_rs_loss --pd_sr_loss --ptcl_d 8e7 --dsvs 0.003

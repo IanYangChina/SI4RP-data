e6cdf638e838e8a83f0f1949b3c0bdc515851584
Xintong <xintongyang1994@foxmail.com>
2024-03-19 09:26:14 +0000

    results

--- scripts/ablate-ptcl_d-4090-0.sh
+++ scripts/ablate-ptcl_d-4090-0.sh
@@ -1,4 +1,4 @@
-python optimise.py --fewshot --n_run 15 --seed 0 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1.5e8 --dsvs 0.0023 --device_mem 8
+# python optimise.py --fewshot --n_run 15 --seed 0 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1.5e8 --dsvs 0.0023 --device_mem 8
 python optimise.py --fewshot --n_run 15 --seed 1 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1.5e8 --dsvs 0.0023 --device_mem 8
 python optimise.py --fewshot --n_run 15 --seed 2 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1.5e8 --dsvs 0.0023 --device_mem 8
 python optimise.py --oneshot --n_run 15 --seed 0 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1.5e8 --dsvs 0.0023 --device_mem 8
--- scripts/ablate-ptcl_d-4090-1.sh
+++ scripts/ablate-ptcl_d-4090-1.sh
@@ -1,7 +1,7 @@
 # python optimise.py --fewshot --n_run 14 --seed 0 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1e8 --dsvs 0.0028 --device_mem 5
-python optimise.py --fewshot --n_run 14 --seed 1 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1e8 --dsvs 0.0028 --device_mem 5
-python optimise.py --fewshot --n_run 14 --seed 2 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1e8 --dsvs 0.0028 --device_mem 5
-python optimise.py --oneshot --n_run 14 --seed 0 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1e8 --dsvs 0.0028 --device_mem 5
+# python optimise.py --fewshot --n_run 14 --seed 1 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1e8 --dsvs 0.0028 --device_mem 5
+# python optimise.py --fewshot --n_run 14 --seed 2 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1e8 --dsvs 0.0028 --device_mem 5
+# python optimise.py --oneshot --n_run 14 --seed 0 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1e8 --dsvs 0.0028 --device_mem 5
 python optimise.py --oneshot --n_run 14 --seed 1 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1e8 --dsvs 0.0028 --device_mem 5
 python optimise.py --oneshot --n_run 14 --seed 2 --backend cuda --param_set 1 --pd_rs_loss --pd_sr_loss --ptcl_d 1e8 --dsvs 0.0028 --device_mem 5
 python optimise.py --realoneshot --n_run 14 --seed 0  --backend cuda --param_set 1 --realoneshot_agent_id 0 --pd_rs_loss --pd_sr_loss --ptcl_d 1e8 --dsvs 0.0028 --device_mem 5

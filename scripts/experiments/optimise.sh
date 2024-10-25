e90b87b3087880f32ff58a4e28b405be2b81e77f
xintong <xintongyang1994@foxmail.com>
2024-10-25 15:16:58 +0100

    delete .vox files

---
+++ scripts/experiments/optimise.sh
@@ -0,0 +1,8 @@
+python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 3
+python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 4
+python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 5
+python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 6
+#python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 7
+#python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 8
+#python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 9
+#python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 10

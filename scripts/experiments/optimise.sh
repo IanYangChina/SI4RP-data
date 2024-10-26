6aead1b256d917b7ec52829efa08e9b89bc80de1
xintong <xintongyang1994@foxmail.com>
2024-10-26 12:18:20 +0100

    new script

--- scripts/experiments/optimise.sh
+++ scripts/experiments/optimise.sh
@@ -2,7 +2,25 @@ python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 3
 python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 4
 python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 5
 python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 6
-#python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 7
-#python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 8
-#python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 9
-#python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 10
+python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 7
+python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 8
+python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 9
+python optimise.py --n_run 3 --con_lv 1 --dataset 1rec --emd_pr_loss --seed 10
+
+python optimise.py --n_run 3 --con_lv 1 --dataset 1round --emd_pr_loss --seed 3
+python optimise.py --n_run 3 --con_lv 1 --dataset 1round --emd_pr_loss --seed 4
+python optimise.py --n_run 3 --con_lv 1 --dataset 1round --emd_pr_loss --seed 5
+python optimise.py --n_run 3 --con_lv 1 --dataset 1round --emd_pr_loss --seed 6
+python optimise.py --n_run 3 --con_lv 1 --dataset 1round --emd_pr_loss --seed 7
+python optimise.py --n_run 3 --con_lv 1 --dataset 1round --emd_pr_loss --seed 8
+python optimise.py --n_run 3 --con_lv 1 --dataset 1round --emd_pr_loss --seed 9
+python optimise.py --n_run 3 --con_lv 1 --dataset 1round --emd_pr_loss --seed 10
+
+python optimise.py --n_run 3 --con_lv 1 --dataset 1cyl --emd_pr_loss --seed 3
+python optimise.py --n_run 3 --con_lv 1 --dataset 1cyl --emd_pr_loss --seed 4
+python optimise.py --n_run 3 --con_lv 1 --dataset 1cyl --emd_pr_loss --seed 5
+python optimise.py --n_run 3 --con_lv 1 --dataset 1cyl --emd_pr_loss --seed 6
+python optimise.py --n_run 3 --con_lv 1 --dataset 1cyl --emd_pr_loss --seed 7
+python optimise.py --n_run 3 --con_lv 1 --dataset 1cyl --emd_pr_loss --seed 8
+python optimise.py --n_run 3 --con_lv 1 --dataset 1cyl --emd_pr_loss --seed 9
+python optimise.py --n_run 3 --con_lv 1 --dataset 1cyl --emd_pr_loss --seed 10

865d86cddf4560a37f6cefbfb2e2095808c8872b
xintong <xintongyang1994@foxmail.com>
2024-10-28 09:28:47 +0000

    new script

---
+++ scripts/experiments/optimise_0.sh
@@ -0,0 +1,11 @@
+python optimise.py --n_run 4 --con_lv 1 --dataset 1rec --hm_loss --seed 0
+python optimise.py --n_run 4 --con_lv 1 --dataset 1rec --hm_loss --seed 1
+python optimise.py --n_run 4 --con_lv 1 --dataset 1rec --hm_loss --seed 2
+
+python optimise.py --n_run 4 --con_lv 1 --dataset 1round --hm_loss --seed 0
+python optimise.py --n_run 4 --con_lv 1 --dataset 1round --hm_loss --seed 1
+python optimise.py --n_run 4 --con_lv 1 --dataset 1round --hm_loss --seed 2
+
+python optimise.py --n_run 4 --con_lv 1 --dataset 1cyl --hm_loss --seed 0
+python optimise.py --n_run 4 --con_lv 1 --dataset 1cyl --hm_loss --seed 1
+python optimise.py --n_run 4 --con_lv 1 --dataset 1cyl --hm_loss --seed 2
